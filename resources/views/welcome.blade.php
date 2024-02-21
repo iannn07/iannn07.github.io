71e20455ffa4c272d52c58977978968811e82bdb
Iannn <pristianb007@gmail.com>
2024-02-22 00:10:34 +0700

    First Commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+    <title>Ian GitHub Pages</title>
+
+    <!-- Favicons -->
+    <link href="{{ asset('img/favicon.jpg') }}" rel="icon">
+
+    <!-- Google Fonts -->
+    <link
+        href="https://fonts.googleapis.com/css?family=Poppins:300,300i,400,400i,500,500i,600,600i,700,700i|Playfair+Display:400,400i,500,500i,600,600i,700,700i,900,900i"
+        rel="stylesheet">
+
+    <!-- Vendor CSS Files -->
+    <link href="{{ asset('vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('vendor/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet">
+    <link href="{{ asset('vendor/boxicons/css/boxicons.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('vendor/glightbox/css/glightbox.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('vendor/swiper/swiper-bundle.min.css') }}" rel="stylesheet">
+
+    <!-- Template Main CSS File -->
+    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+
+    <!-- =======================================================
+  * Template Name: Folio
+  * Updated: Jan 29 2024 with Bootstrap v5.3.2
+  * Template URL: https://bootstrapmade.com/folio-bootstrap-portfolio-template/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+    <!-- ======= Hero Section ======= -->
+    <div id="hero" class="home">
+
+        <div class="container">
+            <div class="hero-content">
+                <h1>Hi, Ian here!</h1>
+                <h1>I'm <span class="typed"
+                        data-typed-items="Data Scientist, Medical Enthusiast, Full-Stack Developer"></span></h1>
+
+                <ul class="list-unstyled list-social">
+                    <li><a href="https://github.com/iannn07"><i class="bi bi-github" style="font-size: 24px"></i></a>
+                    </li>
+                    <li><a href="https://www.linkedin.com/in/pristian-budi-dharmawan/"><i class="bi bi-linkedin"
+                                style="font-size: 24px"></i></a></li>
+                </ul>
+            </div>
+        </div>
+    </div><!-- End Hero -->
+
+    <!-- ======= Footer ======= -->
+    <div id="footer" class="text-center">
+        <div class="container">
+            <div class="socials-media text-center">
+
+                <ul class="list-unstyled">
+                    <li><a href="https://github.com/iannn07"><i class="bi bi-github" style="font-size: 24px"></i></a>
+                    </li>
+                    <li><a href="https://www.linkedin.com/in/pristian-budi-dharmawan/"><i class="bi bi-linkedin"
+                                style="font-size: 24px"></i></a></li>
+                </ul>
+
+            </div>
+
+            <p>&copy; Copyrights Ian. All rights reserved.</p>
+
+            <div class="credits">
+                <!--
+        All the links in the footer should remain intact.
+        You can delete the links only if you purchased the pro version.
+        Licensing information: https://bootstrapmade.com/license/
+        Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/buy/?theme=Folio
+      -->
+                Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+            </div>
+
+        </div>
+    </div><!-- End Footer -->
+
+    <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
+            class="bi bi-arrow-up-short"></i></a>
+
+    <!-- Vendor JS Files -->
+    <script src="{{ asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('vendor/glightbox/js/glightbox.min.js') }}"></script>
+    <script src="{{ asset('vendor/isotope-layout/isotope.pkgd.min.js') }}"></script>
+    <script src="{{ asset('vendor/swiper/swiper-bundle.min.js') }}"></script>
+    <script src="{{ asset('vendor/typed.js/typed.umd.js') }}"></script>
+    <script src="{{ asset('vendor/php-email-form/validate.js') }}"></script>
+
+    <!-- Template Main JS File -->
+    <script src="{{ asset('js/main.js') }}"></script>
+
+</body>
+
+</html>
